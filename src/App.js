a4b5eabf56e695de389936d73ddb0edda56e43e6
kishan <kkseth412@gmail.com>
2023-09-25 15:11:10 +0530

    initial update

--- src/App.js
+++ src/App.js
@@ -1,29 +1,46 @@
-import React from "react";
-import Head from "./components/Head.js";
-import Body from "./components/Body.js";
-import './App.css';
-  
+import "./App.css";
+import Body from "./components/Body";
+import {RouterProvider, createBrowserRouter} from 'react-router-dom'
+import Header from "./components/Header";
+import { Provider } from "react-redux";
+import store from "./utils/store";
+import MainContainer from "./components/MainContainer"
+import WatchPage from "./components/WatchPage";
+import Demo from "./components/Demo";
+import Demo2 from "./components/Demo2";
 
-const App = () =>{
 
-/**
- * 
- * head
- * body
- *    sidebar
- *      menuitems
- *    mainContainer
- *      button List
- *      videocontainer
- *          video card
- * 
- */
+const appRouter = createBrowserRouter([{
+  path: "/",
+  element: <Body/>,
+  children: [
+    {
+      path: "/",
+      element: <MainContainer/>
+    },
+    {
+      path: "/watch",
+      element: <WatchPage/>
+    },
+    {
+      path: "/demo",
+      element: <><Demo/><Demo2/></>,
+    },
 
-    return (
-        <div>
-            <Head />
-            <Body />
-        </div>
-    )
-}
+
+  ]
+
+}])
+
+const App = () => {
+  return (
+    <Provider store={store}>
+      <div>
+        <Header />
+        <RouterProvider router={appRouter} />
+      </div>
+    </Provider>
+  );
+};
+
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDom from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-import App from './App'
-
-const root = ReactDom.createRoot(document.getElementById('root'));
-
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App/>
+    <App />
+  
 );
+
+reportWebVitals();
